dc573b6aa48c3a53eb0645b90781ff6f8808e3dd
Johnathon Wood <jaywood128@gmail.com>
2019-02-15 21:21:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,7 +37,7 @@ class ApplicationController < Sinatra::Base
   end
 
   patch '/articles/:id' do
-    binding.pry
+
     @article = Article.find(params["id"])
     @article.update(params["article"])
 
